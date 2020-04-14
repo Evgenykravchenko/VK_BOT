c393d0f63d6fa5799e055d12e84c8b9f90b2497b
Evgeny Kravchenko <evgenyonechenko@gmail.com>
2020-04-14 13:41:45 +0600

    Changes and refactoring.

---
+++ src/main/java/com/bot/Main.java
@@ -0,0 +1,19 @@
+package com.bot;
+
+import com.petersamokhin.bots.sdk.clients.Group;
+import com.petersamokhin.bots.sdk.objects.Message;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Group group = new Group(194194997, Prop.getDataFromProp("vk_key"));
+
+        group.onSimpleTextMessage(message ->
+                new Message()
+                        .from(group)
+                        .to(message.authorId())
+                        .text("Ведутся работы.")
+                        .send()
+        );
+    }
+}
